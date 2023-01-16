53cbf7085de3d5566137b90991f409a20438086d
Viktor_Kirin <razeone1@gmail.com>
2023-01-16 19:24:44 +0100

    add text

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ console.log('You can ignore single and double quotes like this: \\\'\\\'  \\\" \
 // 1 step - inform
 let alertEnterData = alert ('Please enter data')
 // 2 step - ask name
-let promptUserName = prompt("What's your name?");
+let promptUserName = prompt("What's your name?", "Name");
 // 3 step - show result
 alert('Your name is: ' + promptUserName);
 // 4 step - ask surname
-let promptSurname = prompt("Enter your surname");
+let promptSurname = prompt("Enter your surname", "Surname");
 // 5 step - show result
 alert('Your full name is : ' + promptUserName +" "+  promptSurname);
 // 6 step - ask old
-let promptOld = prompt("How old are you?");
+let promptOld = prompt("How old are you?", "Age");
 // 7 step - show result
 alert('Your full name is : ' + promptUserName +" "+  promptSurname + ", your age is: " + promptOld);
 // 7 step - ask admin
