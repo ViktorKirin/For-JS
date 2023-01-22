c7bdeab71be092d774a9b63dae832ba1ad6c92a8
Viktor_Kirin <razeone1@gmail.com>
2023-01-22 18:03:16 +0100

    add №4

--- script20-4.js
+++ script20-4.js
@@ -1,16 +1,20 @@
 "use strict"
 
-
 function KmToM(km)  {
-    
+    return (km * 1000);
 }
 
 function KmToCm(km) {
-
+    return (km * 100000);
 }
 
 function gerMetric (metric, km, m, cm) {
-    km = 
+    if (metric === 'm') {
+        return m(km);
+    }
+    else if (metric === 'cm') {
+        return cm(km);
+    }
 }
 
 let metric = prompt ("write metric m or cm ", "m") ; 
@@ -21,60 +25,10 @@ if (isNaN(metric) || metric === null ) {
    }
 
 let km = prompt ("write the number of kilometers", "1")
- if (isNaN(km) || km === null ) {
+ if (isNaN(km) || km === null || km < 0 ) {
       km = prompt ("Your number is incorect, please enter corect number" , "1");
       km = Number(km);
      } 
 
-
-gerMetric (metric, km, m, cm);
-alert(gerMetric(metric, km))
-
-// function calculator (anyNumber, count) {
-//  return anyNumber + 3 * count;
-// }
-
-// let anyNumber = prompt ("enter the number","1")
-// console.log(anyNumber);
-// while (isNaN(anyNumber) || anyNumber === null ) {
-//      anyNumber = prompt ("Your number is incorect, please enter corect number" , "1");
-//      anyNumber = Number(anyNumber);
-//     } 
-
-//     let count = prompt ("write the number of operations +3","1")
-// console.log(count);
-// while (isNaN(count) || anyNumber === null) {
-//      count = prompt ("Your number is incorect, please enter corect number" , "1");
-//      count = Number(count);
-//     } 
-
-// calculator (anyNumber, count);
-// // alert(calculator(anyNumber, count))
-// alert(calculator(+anyNumber, +count))
-// let word =  prompt ("enter the CSS value of the Display property" , "");
-// console.log(typeof(word));
-// console.log(Number(word));
-// if (isNaN(word)) {
-//     switch (word) {
-//         case 'block':
-//             alert ("Set 'display: block'")
-//             break;
-//         case 'flex':
-//             alert ("Set 'display: flex'")    
-//             break;
-//         case 'grid':
-//             alert ("Set 'display: grid'")
-//             break;
-//         case 'inline':
-//             alert ("Set 'display: inline'")
-//             break;
-//         case 'none':
-//             alert ("Set 'display: none'")
-//             break;
-//         default:
-//             alert ("Set 'display: inline-block;'")
-//             break;
-//     }
-// } else {
-//     alert ("do not rush =)")
-// }
+// gerMetric (metric, km, m, cm);
+alert(gerMetric(metric, km, KmToM , KmToCm ))
